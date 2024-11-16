1197b95521e6dba74e83427e6803f007669e8278
oweitman <oweitman@gmx.de>
2024-11-16 15:20:34 +0100

    complete check and partly rework, make vis2 compatible

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,8 +2,16 @@ module.exports = {
     semi: true,
     trailingComma: "all",
     singleQuote: false,
-    printWidth: 120,
+    printWidth: 80,
     useTabs: false,
     tabWidth: 4,
     endOfLine: "lf",
+    "overrides": [
+        {
+            "files": "*.md",
+            "options": {
+                "tabWidth": 2
+            }
+        }
+    ]
 };
